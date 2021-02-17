31b0d8583f0c405fdcdaa4b5fb5f4dc48f2d555b
Renan Lima <renanlimabl@gmail.com>
2021-02-17 20:39:04 -0300

    add description

--- index.php
+++ index.php
@@ -4,12 +4,14 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Felippe</title>
+  <title>Master Class Jornada do Propósito e Autorrealização</title>
+  <meta name="description" content="Arte de Acreditar | Master Class Jornada do Propósito e Autorrealização" />
   <link rel="stylesheet" href="style.css">
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/png" href="assets/favicon.png" />
 </head>
 <body>
   <section class="main">
